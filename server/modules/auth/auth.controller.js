aec24eed3d56b558c5468c8f808fbf7e2cf0f04a
Ferreira Pereira <ferreirajuliao@hotmail.com>
2025-04-18 23:15:57 +0100

    Auto push to gitHub

---
+++ server/modules/auth/auth.controller.js
@@ -0,0 +1,28 @@
+import * as authService from '../authService.js';
+
+export async function register(req, res){
+    try{
+        const user = await authService.registerUser(req.body);
+        res.status(201).json(user);
+    }catch(error){
+        res.status(400).json({error: error.message});
+    }
+}
+
+export async function login(req, res){
+    try{
+        const user = await authService.loginUser(req.body);
+        res.status(200).json(user);
+    }catch(error){
+        res.status(400).json({error: error.message});
+    }
+}
+
+export async function logout(req, res){
+    try{
+        const user = await authService.logoutUser(req.body);
+        res.status(200).json(user);
+    }catch(error){
+        res.status(400).json({error: error.message});
+    }
+}
--- server/modules/auth/auth.service.js
+++ server/modules/auth/auth.service.js
@@ -1,2 +1,39 @@
-const allUsers = await prisma.use.findMany();
-console.log(allUsers);
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import prisma from '../../config/prisma.js';
+
+const JWT_SECRET = process.env.JWT_SECRET; 
+
+export async function registerUser({ email, password, name}){
+    const hashed = await bcrypt.hash(password, 10); //funcao para criptografar a senha
+    const user = await prisma.user.create({
+        data: {
+            email,
+            password: hashed,
+            name,
+        },
+    });
+    return { id:user.id, email: user.email, name: user.name };
+    //Ira retomar os dados do usuario criado
+}
+
+export async function loginUser({ email, password}){
+    const user = await prisma.user.findUnique({ where: { email }});
+    if(!user || !(await bcrypt.compare(password, user.password))){
+        throw new Error('Credencias inválidas');
+    }
+    const token = jwt.sign({ 
+        userId: user.id,
+        role:user.role
+    }, JWT_SECRET, { expiresIn: '8h' });
+
+    return {
+        token,
+        user: {
+            id: user.id,
+            email: user.email,
+            name: user.name,
+            role: user.role,
+        }
+    };
+}
